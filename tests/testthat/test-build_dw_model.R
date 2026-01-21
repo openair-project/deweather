6f93f2cae8975e383fc3586539744c07bf2cbec1
jack-davison <davison.jack.jd@gmail.com>
2026-01-21 09:02:22 +0000

    test: expand test set to include ranger

--- tests/testthat/test-build_dw_model.R
+++ tests/testthat/test-build_dw_model.R
@@ -1,79 +1,103 @@
 small_data <- head(aqroadside, n = 1000)
-dw_model <- build_dw_model(small_data, "no2")
 
-test_that("generics work", {
-  expect_no_error(print(dw_model))
-  expect_no_error(head(dw_model))
-  expect_no_error(tail(dw_model))
-  expect_no_error(plot(dw_model))
-  expect_no_error(summary(dw_model))
-})
+# test a boosted tree and random forest model
+for (engine in c("xgboost", "ranger")) {
+  # build the model first
+  dw_model <- build_dw_model(small_data, "no2", engine = engine)
 
-test_that("getters work", {
-  expect_equal(get_dw_engine(dw_model), "xgboost")
-  expect_equal(get_dw_pollutant(dw_model), "no2")
-  expect_equal(
-    get_dw_vars(dw_model),
-    c("trend", "ws", "wd", "hour", "weekday", "air_temp")
-  )
-  expect_equal(
-    names(get_dw_input_data(dw_model)),
-    c("no2", "trend", "ws", "wd", "hour", "weekday", "air_temp")
-  )
-  expect_equal(
-    get_dw_params(dw_model),
-    list(
-      tree_depth = 5,
-      trees = 200L,
-      learn_rate = 0.1,
-      mtry = NULL,
-      min_n = 10L,
-      loss_reduction = 0,
-      sample_size = 1L,
-      stop_iter = 190L
+  # check that the generics are printing properly
+  test_that("generics work", {
+    expect_no_error(print(dw_model))
+    expect_no_error(head(dw_model))
+    expect_no_error(tail(dw_model))
+    expect_no_error(plot(dw_model))
+    expect_no_error(summary(dw_model))
+  })
+
+  # check that we can get the correct part of the deweather object
+  test_that("getters work", {
+    expect_equal(get_dw_engine(dw_model), engine)
+    expect_equal(get_dw_pollutant(dw_model), "no2")
+    expect_equal(
+      get_dw_vars(dw_model),
+      c("trend", "ws", "wd", "hour", "weekday", "air_temp")
     )
-  )
-  expect_equal(get_dw_params(dw_model, "tree_depth"), 5)
+    expect_equal(
+      names(get_dw_input_data(dw_model)),
+      c("no2", "trend", "ws", "wd", "hour", "weekday", "air_temp")
+    )
 
-  imp <- get_dw_importance(dw_model)
-  expect_type(imp$importance, "double")
-  expect_s3_class(imp$var, "factor")
-  expect_equal(nrow(imp), 12)
+    if (engine == "xgboost") {
+      expect_equal(
+        get_dw_params(dw_model),
+        list(
+          tree_depth = 5,
+          trees = 200L,
+          learn_rate = 0.1,
+          mtry = NULL,
+          min_n = 10L,
+          loss_reduction = 0,
+          sample_size = 1L,
+          stop_iter = 190L
+        )
+      )
+    }
 
-  imp2 <- get_dw_importance(dw_model, aggregate_factors = TRUE)
-  expect_equal(nrow(imp2), length(get_dw_vars(dw_model)))
+    if (engine == "ranger") {
+      expect_equal(
+        get_dw_params(dw_model),
+        list(
+          trees = 200L,
+          mtry = NULL,
+          min_n = 10L
+        )
+      )
+    }
 
-  imp3 <- get_dw_importance(dw_model, sort = FALSE)
-  expect_type(imp3$var, "character")
-})
+    expect_equal(get_dw_params(dw_model, "trees"), 200L)
 
-test_that("plots work", {
-  expect_no_error(plot_dw_importance(dw_model))
-  expect_no_error(plot_dw_importance(dw_model, aggregate_factors = TRUE))
+    # importance is more complex
+    imp <- get_dw_importance(dw_model)
+    expect_type(imp$importance, "double")
+    expect_s3_class(imp$var, "factor")
+    expect_equal(nrow(imp), 12)
 
-  expect_s3_class(plot_dw_partial_1d(dw_model, n = 10), "gg")
-  expect_s3_class(plot_dw_partial_1d(dw_model, "hour", n = 10), "gg")
-  expect_s3_class(plot_dw_partial_1d(dw_model, c("hour", "ws"), n = 10), "gg")
-  expect_s3_class(
-    plot_dw_partial_1d(dw_model, c("hour", "ws"), group = "weekday", n = 10),
-    "gg"
-  )
-  expect_s3_class(
-    plot_dw_partial_1d(dw_model, c("hour", "ws"), group = "hour", n = 10),
-    "gg"
-  )
+    imp2 <- get_dw_importance(dw_model, aggregate_factors = TRUE)
+    expect_equal(nrow(imp2), length(get_dw_vars(dw_model)))
 
-  expect_s3_class(plot_dw_partial_2d(dw_model, "hour", "ws", n = 10), "gg")
-  expect_s3_class(
-    plot_dw_partial_2d(dw_model, "hour", "ws", n = 10, contour = "lines"),
-    "gg"
-  )
-  expect_s3_class(
-    plot_dw_partial_2d(dw_model, "hour", "ws", n = 10, contour = "fill"),
-    "gg"
-  )
-  expect_s3_class(
-    plot_dw_partial_2d(dw_model, "hour", "ws", n = 10, show_conf_int = TRUE),
-    "gg"
-  )
-})
+    imp3 <- get_dw_importance(dw_model, sort = FALSE)
+    expect_type(imp3$var, "character")
+  })
+
+  # plotting functions
+  test_that("plots work", {
+    expect_no_error(plot_dw_importance(dw_model))
+    expect_no_error(plot_dw_importance(dw_model, aggregate_factors = TRUE))
+
+    expect_s3_class(plot_dw_partial_1d(dw_model, n = 10), "gg")
+    expect_s3_class(plot_dw_partial_1d(dw_model, "hour", n = 10), "gg")
+    expect_s3_class(plot_dw_partial_1d(dw_model, c("hour", "ws"), n = 10), "gg")
+    expect_s3_class(
+      plot_dw_partial_1d(dw_model, c("hour", "ws"), group = "weekday", n = 10),
+      "gg"
+    )
+    expect_s3_class(
+      plot_dw_partial_1d(dw_model, c("hour", "ws"), group = "hour", n = 10),
+      "gg"
+    )
+
+    expect_s3_class(plot_dw_partial_2d(dw_model, "hour", "ws", n = 10), "gg")
+    expect_s3_class(
+      plot_dw_partial_2d(dw_model, "hour", "ws", n = 10, contour = "lines"),
+      "gg"
+    )
+    expect_s3_class(
+      plot_dw_partial_2d(dw_model, "hour", "ws", n = 10, contour = "fill"),
+      "gg"
+    )
+    expect_s3_class(
+      plot_dw_partial_2d(dw_model, "hour", "ws", n = 10, show_conf_int = TRUE),
+      "gg"
+    )
+  })
+}
